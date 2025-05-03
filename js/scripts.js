6b3334515fb901a1de22de70a75ce4d270431bcd
Syllicone <daniel.sinclair5@gmail.com>
2025-05-03 14:53:01 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pages = document.querySelectorAll('.page');
-
-    // Add event listeners to SVG elements
-    document.getElementById('home-trigger').addEventListener('click', () => {
-        navigateToPage('home');
-    });
-
-    document.getElementById('work-trigger').addEventListener('click', () => {
-        navigateToPage('work');
-    });
-
-    document.getElementById('about-trigger').addEventListener('click', () => {
-        navigateToPage('about');
-    });
-
-    document.getElementById('contact-trigger').addEventListener('click', () => {
-        navigateToPage('contact');
-    });
-
-    function navigateToPage(target) {
-        pages.forEach(page => {
-            page.classList.toggle('hidden', page.id !== target);
-        });
-    }
+document.getElementById('home-trigger').addEventListener('click', () => {
+    showPage('home-page');
 });
+
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(page => {
+        page.classList.add('hidden');
+    });
+    document.getElementById(pageId).classList.remove('hidden');
+}
