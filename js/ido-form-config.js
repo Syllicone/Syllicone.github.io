aa51ede955b02bc42aa06a5c25a069f94f83524e
Syllicone <daniel.sinclair5@gmail.com>
2026-05-25 14:12:57 +0100

    Update ido-form-config.js

--- js/ido-form-config.js
+++ js/ido-form-config.js
@@ -1,3 +1,3 @@
 // Get a free access key at https://web3forms.com (use dsinc001@campus.goldsmiths.ac.uk).
 // Web3Forms will email you the key — paste it below. This key is safe to be public.
-window.IDO_WEB3FORMS_ACCESS_KEY = '';
+window.IDO_WEB3FORMS_ACCESS_KEY = '31c73985-05c5-4082-8ec5-0eaae62c6220';
